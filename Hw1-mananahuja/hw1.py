2a7e1dbfa144c9e4064ae50e17aefc0ad3e5883d
mananahujaa <63778460+mananahujaa@users.noreply.github.com>
2023-08-25 08:58:26 -0400

    Update hw1.py

--- Hw1-mananahuja/hw1.py
+++ Hw1-mananahuja/hw1.py
@@ -5,14 +5,47 @@ possible values that are allowed.
 - Dr. Licato"""
 
 import random
-
+import re
 
 def problem1(NPs, s):
 	hypernyms = set()
+    
+    # Create regular expression patterns based on Hearst patterns
+	patterns = [
+		r'(\b{}\b).+?(\bis a\b).+?(\b{}\b)'.format(np1, np2)
+		for np1 in NPs
+		for np2 in NPs
+		if np1 != np2
+	]
 	for i in range(len(NPs)-1):
 		hypernyms.add( (NPs[i], NPs[i+1]) )
+	# Compile the patterns
+	compiled_patterns = [re.compile(pattern, re.IGNORECASE) for pattern in patterns]
+    
+    # Search for patterns in the text and extract hypernyms
+	for pattern in compiled_patterns:
+		matches = pattern.findall(s)
+		for match in matches:
+			hypernym = (match[2], match[0])
+			hypernyms.add(hypernym)
 	return hypernyms
 
 
 def problem2(s1, s2):
+	m , n = len(s1) , len(s2)
+	dx = [[0] * (n + 1) for _ in range (m + 1)]
+
+	for i in range(m+1):
+		dx[i][0] = i
+	
+	for j in range(n+1):
+		dx[0][j] = j
+	for i in range(1, m+1):
+		for j in range (1, n+1):
+			if s1[i-1] == s2[j-1]:
+				dx[i][j]==dx[i-1][j-1]
+			else:
+				dx[i][j]=1+min(dx[i-1][j],dx[i][j-1],dx[i-1][j-1])
+
 	return random.randint(0, abs(len(s1) - len(s2)))
+
